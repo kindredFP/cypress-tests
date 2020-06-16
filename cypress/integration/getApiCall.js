ab606d28628d6e8bc1a3d364df1a3666e0dbec7d
kindred_fp <kindredfp@users.noreply.github.com>
2020-06-15 22:25:23 -0400

    removed .DS_Store file and updated an inner field check

--- cypress/integration/getApiCall.js
+++ cypress/integration/getApiCall.js
@@ -38,12 +38,45 @@ describe("/posts API", () => {
       .should('have.property', 'title','sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
   })
 
-
-/*
-    it('Force a test failure', () => {
-   cy.request('POST', 'https://postman-echo.com/post').its('status')
-      .should('equal', 204);
+  it('Individual Fields and Value Check', () => {
+    cy.request('posts/1')
+      .its('body')
+      .should('have.property', 'title','sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
   })
-*/
 
+  it('Inner Fields Check', () => {
+    cy.request('users/1')
+      .its('body')
+      .then((response) => {
+          //expect(response.body).to.have.property('errors[0].status')
+          const innerJson = response.address.geo;
+          expect(innerJson).to.have.property('lat')
+        })
+  })
+  /****
+  Sample json with inner fields
+  {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+  ****/
 });
