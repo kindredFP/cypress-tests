45892c1cae8d6d0d771454e4cd175da9cf02d8b5
kindred_fp <kindredfp@users.noreply.github.com>
2020-07-18 22:39:03 -0400

    Add to UI tests

--- cypress/integration/ui_Test.js
+++ cypress/integration/ui_Test.js
@@ -17,10 +17,20 @@ describe('My UI Test', () => {
 
   })
 
-  /*
-  it('Navigate and click on button', () => {
+  it('Navigate and use drop down', () => {
+    cy.get(':nth-child(3) > :nth-child(1) > .tree-indicator').click()
+    cy.get(':nth-child(3) > :nth-child(1) > ul > :nth-child(4) > a').click()
 
+    // Selecting the dropdown value
+    cy.get('#select-demo').select('Friday')
+    cy.get('.selected-value').contains('Friday')
   })
-  */
 
+  it('Navigate and access tables', () => {
+    cy.get(':nth-child(3) > :nth-child(3) > [href="#"]').click()
+    cy.get(':nth-child(3) > :nth-child(3) > ul > :nth-child(2) > a').click()
+
+    cy.get('#task-table > tbody > :nth-child(4) > :nth-child(3)').contains('Emily John')
+  })
+  
 })
